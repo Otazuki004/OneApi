0d5d9dec49fca8897a40fdf2fa11f5f47395aeaf
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 15:21:22 +0530

    Update start.py

--- OneApi/pyro/start.py
+++ OneApi/pyro/start.py
@@ -1,6 +1,6 @@
 from .. import *
 from pyrogram import *
 
-@app.on_message(filters.command('start'))
+@bot.on_message(filters.command('start') & filters.user(DEVS))
 async def start(_, message):
   await message.reply("Hmm I'm alive probably")
