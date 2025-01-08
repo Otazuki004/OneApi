6d8826f3ef1d35d18cc7d7cd4ea39339b8392d0d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 11:48:43 +0530

    Update __init__.py

--- OneApi/__init__.py
+++ OneApi/__init__.py
@@ -1,1 +1,18 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
+import logging
+from variables import *
+import os
+from quart import Quart
 
+logging.basicConfig(
+  format="[OneApi] %(name)s - %(levelname)s - %(message)s",
+  handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+  level=logging.INFO,
+)
+
+
+MONGO_DB_URL = os.environ.get("MONGO_DB_URL") or VAR_MONGO_DB_URL
+
+DATABASE = AsyncIOMotorClient(MONGO_DB_URL)["OneApi"]
+app = Quart(__name__)
