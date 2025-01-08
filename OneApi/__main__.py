f242120eebf6b7afac7499ab1f8cb0471c4f7fa1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 21:03:26 +0530

    Update __main__.py

--- OneApi/__main__.py
+++ OneApi/__main__.py
@@ -17,43 +17,15 @@ async def run_quart():
     await app.run_task(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
     print("Quart stopped.")
 
-async def run_bot():
-    print("Starting Pyrogram bot...")
-    await bot.start()
-    print("Bot started successfully")
-    await idle()
-    print("Idle isn't idling ")
-
-async def shutdown():
-    print("Shutting down...")
-    await bot.stop()
-    print("Bot stopped.")
-    print("Shutdown complete.")
-
 async def main():
-    # Concurrently run both Pyrogram and Quart
-    tasks = [run_bot(), run_quart()]
-    try:
-        await asyncio.gather(*tasks)
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        await shutdown()
-
-if __name__ == '__main__':
-    # Create the asyncio event loop
+    print("Main triggered")
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+    loop.run_until_complete(run_quart())
 
-    # Add signal handlers for shutdown
-    for signal in [SIGINT, SIGTERM]:
-        loop.add_signal_handler(signal, lambda: asyncio.create_task(shutdown()))
-
-    try:
-        print("Starting the application...")
-        loop.run_until_complete(main())
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("Finalizing shutdown...")
-        loop.run_until_complete(shutdown())
-        loop.close()
+if __name__ == '__main__':
+    print("Starting bot")
+    bot.start()
+    asyncio.create_task(main())
+    idle()
+    print("Bot stoped")
