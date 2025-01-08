0578f6debacd896cf22d63128b0e2b5bb7636142
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 16:16:35 +0530

    Update __main__.py

--- OneApi/__main__.py
+++ OneApi/__main__.py
@@ -1,19 +1,20 @@
 from quart_cors import cors
 from quart import *
-from . import *
-import os
 from pyrogram import *
+import os
 import asyncio
+from . import *
 
 app = cors(app, allow_origin="*")
 
 @app.route('/')
 def home():
-  return jsonify({'success': 'server online'})
+    return jsonify({'success': 'server online'})
 
 async def run():
-  t = asyncio.create_task(bot.start())
-  b = asyncio.create_task(app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))))
-  await idle()
+    asyncio.create_task(bot.start())
+    asyncio.create_task(app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080))))
+    await idle()
+
 if __name__ == '__main__':
-  asyncio.run(run())
+    asyncio.run(run())
