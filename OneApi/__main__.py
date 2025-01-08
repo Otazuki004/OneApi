ede10e5e791b53f3d94b43a127db6ec8a86fd879
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 15:39:13 +0530

    Update __main__.py

--- OneApi/__main__.py
+++ OneApi/__main__.py
@@ -11,7 +11,11 @@ app = cors(app, allow_origin="*")
 def home():
   return jsonify({'success': 'server online'})
 
+async def run():
+    await asyncio.gather(
+        bot.start(),
+        app.run_task(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    )
+    await idle()
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 8080))
-  bot.start()
-  asyncio.gather(app.run(debug=True, host="0.0.0.0", port=port), idle())
+  asyncio.run(run())
