b74189617dd2056ca8c67e99fe4ff9d2d667b51f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 11:51:45 +0530

    Update __main__.py

--- OneApi/__main__.py
+++ OneApi/__main__.py
@@ -1,1 +1,14 @@
+from quart_cors import cors
+from quart import *
+from . import *
+import os
 
+app = cors(app, allow_origin="*")
+
+@app.route('/')
+def home():
+  return jsonify({'success': 'server online'})
+
+if __name__ == '__main__':
+  port = int(os.environ.get("PORT", 8080))
+  app.run(debug=True, host="0.0.0.0", port=port)
