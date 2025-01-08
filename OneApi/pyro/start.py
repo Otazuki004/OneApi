17ca7816945a5f77a2675727ae8630350e57f9d9
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 16:03:09 +0530

    Update start.py

--- OneApi/pyro/start.py
+++ OneApi/pyro/start.py
@@ -1,6 +1,9 @@
 from .. import *
 from pyrogram import *
+import logging 
+import traceback 
 
 @Client.on_message(filters.command('start'))
 async def start(_, message):
-  await message.reply("Hmm I'm alive probably")
+  try: await message.reply("Hmm I'm alive probably")
+  except: logging.error(traceback.format_exc())
