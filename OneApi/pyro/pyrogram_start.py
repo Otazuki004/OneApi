051f90a8782abf9b71c89ca86be7a6f6de5af6e7
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 21:53:09 +0530

    Update pyrogram_start.py

--- OneApi/pyro/pyrogram_start.py
+++ OneApi/pyro/pyrogram_start.py
@@ -2,24 +2,13 @@ from pyrogram import Client, idle
 from .. import *
 import logging
 import asyncio
+import time
+import threading
 
-async def start():
-  await asyncio.sleep(2.5)
+def start():
+  time.sleep(2.5)
   logging.info("Starting pyrogram...")
-  while True:
-    try:
-      await bot.start()
-      await asyncio.sleep(0.5)
-    except: pass
+  bot.run()
 
-async def main():
-    await start()
-
-def run_main():
-    loop = asyncio.get_event_loop()
-    if loop.is_running():
-        asyncio.ensure_future(main())
-    else:
-        loop.run_until_complete(main())
-
-run_main()
+oh = threading.Thread(target=start)
+oh.start()
