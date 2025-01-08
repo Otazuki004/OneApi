704cc163140b1466f44dbe65faabc42fc86ebefe
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 15:15:32 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,5 @@
+VAR_TOKEN = ""
+VAR_MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
+VAR_API_ID = 29764570
+VAR_API_HASH = "7676192e491329a1b859b2184e5d7823"
+VAR_DEVS = [5965055071, 6632519077] 
