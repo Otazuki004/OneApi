ff19b2f7207b72bc0ca2bc0355bb64fb1781d9f5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 16:39:10 +0530

    Update __main__.py

--- OneApi/__main__.py
+++ OneApi/__main__.py
@@ -2,22 +2,22 @@ from quart_cors import cors
 from quart import *
 from pyrogram import Client
 import os
-import asyncio
-from . import *
+import threading
 
 app = cors(app, allow_origin="*")
 
 @app.route('/')
 async def home():
-    return jsonify({'success': 'server online'})
+    return {'success': 'server online'}
 
-async def run_quart():
-    await app.run_task(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+def run_quart():
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
-async def main():
-    await bot.start()  # Start the Pyrogram bot
-    asyncio.create_task(run_quart())
-    await idle()
-    
+def run_pyrogram():
+    bot.start()
+    idle()
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    quart_thread = threading.Thread(target=run_quart)
+    quart_thread.start()
+    run_pyrogram()
