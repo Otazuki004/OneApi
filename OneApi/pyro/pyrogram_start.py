de03399c445a6ce3a877b8d80715de972e110c6b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 21:57:59 +0530

    Update pyrogram_start.py

--- OneApi/pyro/pyrogram_start.py
+++ OneApi/pyro/pyrogram_start.py
@@ -6,9 +6,11 @@ import time
 import threading
 
 def start():
-  time.sleep(2.5)
-  logging.info("Starting pyrogram...")
-  bot.run()
+    time.sleep(2.5)
+    logging.info("Starting pyrogram...")
+    loop = asyncio.new_event_loop()  # Create a new event loop
+    asyncio.set_event_loop(loop)    # Set it as the current event loop
+    bot.run()  # Run the bot within this loop
 
 oh = threading.Thread(target=start)
 oh.start()
