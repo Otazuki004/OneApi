b9819c0fdde2803b32ad59a12956c88ad32f245e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 12:44:36 +0530

    Update user.py

--- OneApi/database/user.py
+++ OneApi/database/user.py
@@ -1,13 +1,20 @@
 from .. import DATABASE
 import logging
+import traceback 
 
 db = DATABASE['user']
 
 class user:
-    async def find(self, user_id):
+    async def find(self, user_id: int):
+      try:
         user = await db.find_one({"_id": user_id})
         return user
+      except:
+        e = traceback.format_exc()
+        logging.error(e)
+        return f"Error: {e}"
     async def add(self, name: str, user_id: int):
+      try:
         if await self.find(user_id): return "exists"
         await db.update_one({"_id": 1}, {"$addToSet": {"users": user_id}}, upsert=True)
         await db.update_one(
@@ -15,3 +22,8 @@ class user:
             {"$set": {"name": name, "coins": 1000000, "projects": [{"name": "Basic Project", "project_id": 1}]}},
             upsert=True
         )
+        return 'ok'
+      except:
+        e = traceback.format_exc()
+        logging.error(e)
+        return f"Error: {e}"
