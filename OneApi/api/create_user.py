5aa9e2a40012a88cddb55a648d067a9aa622670c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 14:40:40 +0530

    Update create_user.py

--- OneApi/api/create_user.py
+++ OneApi/api/create_user.py
@@ -1,1 +1,16 @@
+from ..database import *
+from quart import *
+from .. import *
 
+user = user()
+create_user_bp = Blueprint('create_user', __name__)
+
+@app.route('/exists/', methods=['POST'])
+async def exists():
+  data = await request.get_json()
+  if not data or not 'user_id' in data and not 'name' in data: return jsonify({'error': 'missing user_id & name'}), 400
+  user_id = int(data.get('user_id'))
+  d = await user.find(user_id)
+  if not d: return jsonify({'message': "user not found"}), 404
+  elif 'error' in d: return jsonify({'error': d}), 400
+  return jsonify({'message': 'user exists'}), 200
