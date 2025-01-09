d63da24c84f9941bac977cfc118c45c7b96929a4
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 12:23:38 +0530

    Update and rename users.py to user.py

---
+++ OneApi/database/user.py
@@ -0,0 +1,17 @@
+from .. import DATABASE
+import logging
+
+db = DATABASE['user']
+
+class User:
+    async def find(self, user_id):
+        user = await db.find_one({"_id": user_id})
+        return user
+    async def add(self, name: str, user_id: int):
+        if await self.find(user_id): return "exists"
+        await db.update_one({"_id": 1}, {"$addToSet": {"users": user_id}}, upsert=True)
+        await db.update_one(
+            {"_id": user_id},
+            {"$set": {"name": name, "coins": 1000000, "projects": [{"name": "Basic Project", "project_id": 1}]}},
+            upsert=True
+        )
