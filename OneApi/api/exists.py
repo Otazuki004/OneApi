d0b94578d53a54ed910fba41013133c43d069ec1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 13:22:10 +0530

    Update exists.py

--- OneApi/api/exists.py
+++ OneApi/api/exists.py
@@ -9,7 +9,7 @@ exists_bp = Blueprint('exists', __name__)
 async def exists():
   data = await request.get_json()
   if not data or not 'user_id' in data: return jsonify({'error': 'missing user_id'}), 400
-  user_id = int(data.get(user_id))
+  user_id = int(data.get('user_id'))
   d = await user.find(user_id)
   if not d: return jsonify({'message': "user not found"}), 404
   elif 'error' in d: return jsonify({'error': d}), 400
