81dcf3ba9f485b8815297210c075fdae009cf611
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 15:41:34 +0530

    Update __main__.py

--- OneApi/__main__.py
+++ OneApi/__main__.py
@@ -7,9 +7,11 @@ from . import *
 # BLUEPRINTS _____________________
 from .api.exists import exists_bp
 from .api.create_user import create_user_bp
+from .api.get_projects import get_projects_bp
 
 app.register_blueprint(exists_bp)
 app.register_blueprint(create_user_bp)
+app.register_blueprint(get_projects_bp)
 # ---------------------------------
 
 app = cors(app, allow_origin="*")
