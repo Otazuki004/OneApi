488886baad8e6891dd49e00c755d60ec5257ed40
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 22:36:32 +0530

    Update user.py

--- OneApi/database/user.py
+++ OneApi/database/user.py
@@ -19,7 +19,7 @@ class user:
         await db.update_one({"_id": 1}, {"$addToSet": {"users": user_id}}, upsert=True)
         await db.update_one(
             {"_id": user_id},
-            {"$set": {"name": name, "coins": 1000000, "projects": []}},
+            {"$set": {"name": name, "coins": 1000000, "projects": [], 'latest_project': 0}},
             upsert=True
         )
         return 'ok'
@@ -38,3 +38,36 @@ class user:
         e = traceback.format_exc()
         logging.error(e)
         return f"Error: {oh}"
+    async def create_project(self, name: str, user_id: int):
+      try:
+        if not await self.find(user_id): return "not exists"
+        elif not name and len(name) < 4:
+          return 'Name too short'
+        user = await self.find(user_id)
+        name, latest_project = name[:15], user.get('latest_project') + 1
+        
+        if any(proj.get('name') == name for proj in user.get('projects', [])):
+          return "Name already used"
+        
+        await db.update_one(
+          {"_id": f'{user_id}{latest_project}'},
+          {"$set": {
+            "name": name,
+            "id": latest_project,
+            "data": {}
+          }},
+          upsert=True
+        )
+        await db.update_one(
+          {"_id": user_id},
+          {"$addToSet": {
+            "projects": {
+              'name': name,
+              'project_id': latest_project
+            }
+          }}
+        )
+        await db.update_one({"_id": user_id}, {"$set": {"latest_project": latest_project}})
+      except Exception as e:
+        logging.error(traceback.format_exc())
+        return f'Error: {e}'
