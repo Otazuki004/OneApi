773fe3a04cf12dd6b34bf37083dc84d32904a20a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 15:46:02 +0530

    Update get_projects.py

--- OneApi/api/get_projects.py
+++ OneApi/api/get_projects.py
@@ -10,7 +10,7 @@ async def get_projects():
   data = await request.get_json()
   if not data or not 'user_id' in data: return jsonify({'error': 'missing user_id'}), 400
   user_id = int(data.get('user_id'))
-  ohk = await user.get_projects(user_id):
+  ohk = await user.get_projects(user_id)
   if ohk == "projects not found":
     return jsonify({"message": ohk}), 404
   elif 'Error' in ohk:
