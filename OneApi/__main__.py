7f41219e8ff5bba9a70726b426ae8153a5d3bff2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-10 18:21:17 +0530

    Update __main__.py

--- OneApi/__main__.py
+++ OneApi/__main__.py
@@ -10,12 +10,14 @@ from .api.create_user import create_user_bp
 from .api.get_projects import get_projects_bp
 from .api.create_project import create_project_bp 
 from .api.delete_project import delete_project_bp
+from .api.user_info import user_info_bp
 
 app.register_blueprint(exists_bp)
 app.register_blueprint(create_user_bp)
 app.register_blueprint(get_projects_bp)
 app.register_blueprint(create_project_bp)
 app.register_blueprint(delete_project_bp)
+app.register_blueprint(user_info_bp)
 # ---------------------------------
 
 app = cors(app, allow_origin="*")
