2a84397d03c7eecb73628094f769d83a77f89e80
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-10 17:23:08 +0530

    Update create_project.py

--- OneApi/api/create_project.py
+++ OneApi/api/create_project.py
@@ -8,14 +8,15 @@ create_project_bp = Blueprint('create_project', __name__)
 @app.route('/create_project/', methods=['POST'])
 async def create_project():
   data = await request.get_json()
-  if not data or not 'user_id' in data and not 'name' in data:
+  if not data or not 'user_id' in data and not 'name' in data and not 'plan' in data:
     return jsonify({'error': 'missing user_id or name'}), 400
   user_id = int(data.get('user_id'))
   name = data.get('name')
-  mano = await user.create_project(name, user_id)
-  if mano == 'Name too short' or mano == 'Name already used':
-    return jsonify({'message': mano}), 400
-  elif mano == 'ok':
+  plan = data.get('plan')
+  mano = await user.create_project(name, user_id, plan)
+  if mano == 'ok':
     return jsonify({'message': 'Successfully created'}), 200
   elif 'Error' in mano:
     return jsonify({'error': mano}), 400
+  else:
+    return jsonify({'message': mano}), 400
