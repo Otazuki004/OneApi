c58eab14e2f83e44e90f891ff0e60078e93dd56f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 18:14:50 +0530

    Update exists.py

--- OneApi/api/exists.py
+++ OneApi/api/exists.py
@@ -10,7 +10,7 @@ async def exists():
   data = await request.get_json()
   if not data or not 'user_id' in data: return jsonify({'error': 'missing user_id'}), 400
   user_id = int(data.get('user_id'))
-  d = await user.find(user_id)
+  d = await user.find(user_id, check=True)
   if not d: return jsonify({'message': "user not found"}), 404
   elif 'error' in d: return jsonify({'error': d}), 400
   return jsonify({'message': 'user exists'}), 200
