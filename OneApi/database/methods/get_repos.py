7f06679c343923aeec04c76aa851e07c9e4ec6d0
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 23:59:21 +0530

    Update get_repos.py

--- OneApi/database/methods/get_repos.py
+++ OneApi/database/methods/get_repos.py
@@ -3,33 +3,43 @@ import logging
 import httpx 
 
 class GetRepos:
-  async def get_repos(self, user_id: int):
-    try:
-      db, cb = self.db, self.cb
-      user = await self.find(user_id)
-      if not user: return 'not exists'
-      token = (await cb.find_one({"_id": int(user_id)})).get('token', None)
-      if not token: return "not exists"
-    
-      url = "https://api.github.com/installation/repositories"
-      headers = {
-        "Authorization": f"Bearer {token}",
-        "Accept": "application/vnd.github+json"
-      }
-      async with httpx.AsyncClient() as mano:
-        r = await mano.get(url, headers=headers)
-        if r.status_code == 200:
-          data = r.json()
-          if not data["repositories"]: return []
-          ily = []
-          for x in data["repositories"]:
-            name, id = x.get('name'), x.get('id')
-            ily.append({'name': name, 'id': id})
-          logging.info(f"MAIN {r.headers.get('Link', '')}")
-          return ily
-        else:
-          logging.info(f"Failed to get repos of user: {user_id}: {r.text}")
-          return "failed"
-    except Exception as w:
-      logging.error(traceback.format_exc())
-      return f"Error: {w}"
+    async def get_repos(self, user_id: int):
+        try:
+            db, cb = self.db, self.cb
+            user = await self.find(user_id)
+            if not user: return 'not exists'
+            token = (await cb.find_one({"_id": int(user_id)})).get('token', None)
+            if not token: return "not exists"
+        
+            url = "https://api.github.com/installation/repositories"
+            headers = {
+                "Authorization": f"Bearer {token}",
+                "Accept": "application/vnd.github+json"
+            }
+            repos = []
+            
+            while url:
+                async with httpx.AsyncClient() as mano:
+                    r = await mano.get(url, headers=headers)
+                    if r.status_code == 200:
+                        data = r.json()
+                        if not data["repositories"]: 
+                            return []
+                        
+                        for x in data["repositories"]:
+                            name, id = x.get('name'), x.get('id')
+                            repos.append({'name': name, 'id': id})
+                        
+                        link_header = r.headers.get('Link', '')
+                        if 'rel="next"' in link_header:
+                            url = (link_header.replace('\n', '')).split(',')[0].split(';')[0][1:-1]
+                        else:
+                            url = None
+                    else:
+                        logging.info(f"Failed to get repos of user: {user_id}: {r.text}")
+                        return "failed"
+            
+            return repos
+        except Exception as w:
+            logging.error(traceback.format_exc())
+            return f"Error: {w}"
