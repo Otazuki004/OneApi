6d0231f0c76fc06d5f8f16cf9b0b1bdcaae3d98d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 23:09:40 +0530

    Update get_repos.py

--- OneApi/database/methods/get_repos.py
+++ OneApi/database/methods/get_repos.py
@@ -16,20 +16,26 @@ class GetRepos:
         "Authorization": f"Bearer {token}",
         "Accept": "application/vnd.github+json"
       }
+      ily = []
       async with httpx.AsyncClient() as mano:
-        r = await mano.get(url, headers=headers)
-        if r.status_code == 200:
-          data = r.json()
-          if not data["repositories"]: return []
-          ily = []
-          for x in data["repositories"]:
-            name, id = x.get('name'), x.get('id')
-            ily.append({'name': name, 'id': id})
-          return ily
-        else:
-          logging.info(f"Failed to get repos of user: {user_id}: {r.text}")
-          return "failed"
+        while url:
+          r = await mano.get(url, headers=headers)
+          if r.status_code == 200:
+            data = r.json()
+            if not data["repositories"]: return 
+            for x in data["repositories"]:
+              name, id = x.get('name'), x.get('id')
+              ily.append({'name': name, 'id': id})
+            if "Link" in r.headers:
+              links = r.headers["Link"]
+              next_link = [link.split(";")[0].strip("<>") for link in links.split(",") if 'rel="next"' in link]
+              url = next_link[0] if next_link else None
+            else:
+              break
+          else:
+            logging.info(f"Failed to get repos of user: {user_id}: {r.text}")
+            return "failed"
+      return ily
     except Exception as w:
       logging.error(traceback.format_exc())
       return f"Error: {w}"
-        
