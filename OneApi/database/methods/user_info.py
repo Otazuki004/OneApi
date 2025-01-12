1bf888abfcfc6f29e05e89e2ff75925e30697eb6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-12 18:39:38 +0530

    Update user_info.py

--- OneApi/database/methods/user_info.py
+++ OneApi/database/methods/user_info.py
@@ -1,5 +1,6 @@
 import traceback
 import logging
+import httpx 
 
 class UserInfo:
   async def user_info(self, user_id):
@@ -7,11 +8,11 @@ class UserInfo:
       from ..user import db
       user = await self.find(user_id)
       if not user: return "not exists"
-
       data = {
         "name": user.get('name'),
         "coins": user.get('coins'),
-        "projects": len(user.get('projects'))
+        "projects": len(user.get('projects')),
+        "git": user.get('git_name')
       }
       return data
     except Exception as w:
