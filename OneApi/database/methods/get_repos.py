4b82c76a9d99223f109432a11d4a12c397ab6951
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-13 00:55:31 +0530

    Update get_repos.py

--- OneApi/database/methods/get_repos.py
+++ OneApi/database/methods/get_repos.py
@@ -1,6 +1,6 @@
 import traceback
 import logging
-import httpx
+import httpx 
 
 class GetRepos:
   async def get_repos(self, user_id: int):
@@ -16,34 +16,19 @@ class GetRepos:
         "Authorization": f"Bearer {token}",
         "Accept": "application/vnd.github+json"
       }
-      repos = []
-      while url:
-        async with httpx.AsyncClient() as mano:
-          r = await mano.get(url, headers=headers)
-          if r.status_code == 200:
-            data = r.json()
-            if not data["repositories"]: return []
-            for x in data["repositories"]:
-              name, id = x.get('name'), x.get('id')
-              repos.append({'name': name, 'id': id})
-
-            link_header = r.headers.get('Link', '')
-            next_page_url = None
-
-            # Extract next page URL from the 'Link' header
-            if 'rel="next"' in link_header:
-              next_page_url = link_header.split(',')[1].split(';')[0][1:-1]
-              print(next_page_url)
-
-            if not next_page_url:
-              url = None
-            else:
-              url = next_page_url  # Set URL to next page's URL
-          else:
-            logging.info(f"Failed to get repos of user: {user_id}: {r.text}")
-            return "failed"
-    
-      return repos
+      async with httpx.AsyncClient() as mano:
+        r = await mano.get(url, headers=headers)
+        if r.status_code == 200:
+          data = r.json()
+          if not data["repositories"]: return []
+          ily = []
+          for x in data["repositories"]:
+            name, id = x.get('name'), x.get('id')
+            ily.append({'name': name, 'id': id})
+          return ily
+        else:
+          logging.info(f"Failed to get repos of user: {user_id}: {r.text}")
+          return "failed"
     except Exception as w:
       logging.error(traceback.format_exc())
       return f"Error: {w}"
