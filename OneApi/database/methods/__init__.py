2719e5aaa4c4ca8db1d2bbf1d02a19d6d7589ce0
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-14 15:06:50 +0530

    Update __init__.py

--- OneApi/database/methods/__init__.py
+++ OneApi/database/methods/__init__.py
@@ -1,10 +1,12 @@
 from .user_info import UserInfo
 from .get_repos import GetRepos
 from .delete_user import DeleteUser
+from .disconnect_git import DisconnectGit
 
 class Methods(
   UserInfo,
   GetRepos,
   DeleteUser,
+  DisconnectGit,
 ):
   pass
