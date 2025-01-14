d117da697b15f9489c5a95001d3cf5ed5d01b486
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-14 17:48:35 +0530

    Update set_repo.py

--- OneApi/database/methods/set_repo.py
+++ OneApi/database/methods/set_repo.py
@@ -2,5 +2,11 @@ import traceback
 import logging
 
 class SetRepo:
-  async def set_repo(self, user_id: int):
-    pass
+  async def set_repo(self, user_id: int, project_id: int, repo_id: int):
+    db, cb = self.db, self.cb
+    user = await self.find(user_id)
+    project = await db.find_one({"_id": f"{user_id}{project_id}"})
+    if not user: return 'not exists'
+    elif not project: return 'Project not found'
+    
+    
