4def28e18879fa5c9365e55db0250dff5136bfc6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-14 18:44:04 +0530

    Update set_repo.py

--- OneApi/database/methods/set_repo.py
+++ OneApi/database/methods/set_repo.py
@@ -3,30 +3,35 @@ import logging
 
 class SetRepo:
   async def set_repo(self, user_id: int, project_id: int, repo_id: int):
-    db, cb = self.db, self.cb
-    user = await self.find(user_id)
-    project = await db.find_one({"_id": f"{user_id}{project_id}"})
-    if not user: return 'not exists'
-    elif not project: return 'Project not found'
+    try:
+      db, cb = self.db, self.cb
+      user = await self.find(user_id)
+      project = await db.find_one({"_id": f"{user_id}{project_id}"})
+      if not user: return 'not exists'
+      elif not project: return 'Project not found'
 
-    hmm = await self.get_repos(user_id)
-    if not hmm: return 'Repo not found'
+      hmm = await self.get_repos(user_id)
+      if not hmm: return 'Repo not found'
 
-    yes = False
-    for x in hmm:
-      if int(x.get('id')) == project_id:
-        yes = True
-        break
-    if not yes: return 'Repo not found'
+      yes = False
+      for x in hmm:
+        if int(x.get('id')) == project_id:
+          yes = True
+          break
+      if not yes: return 'Repo not found'
 
-    log = project.get('logs')
-    log += f"{self.lf}: Repo linked successfully"
+      log = project.get('logs')
+      log += f"{self.lf}: Repo linked successfully"
 
-    await db.update_one(
-      {"_id": f"{user_id}{project_id}"},
-      {"$set": {
-        "logs": log,
-        "repo": repo_id
-      }}
-    )
+      await db.update_one(
+        {"_id": f"{user_id}{project_id}"},
+        {"$set": {
+          "logs": log,
+          "repo": repo_id
+        }}
+      )
+      return 'ok'
+    except Exception as e:
+      logging.error(traceback.format_exc())
+      return f"Error: {e}"
     
