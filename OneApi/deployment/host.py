d29b33b9eede7e076769f283cd47e6250dac7d58
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-15 12:29:02 +0530

    Update host.py

--- OneApi/deployment/host.py
+++ OneApi/deployment/host.py
@@ -10,15 +10,19 @@ class Host:
     if not user: return 'not exists'
     elif not project: return 'Project not found'
     repo_id = project.get('repo', 0)
-    if not await database.check_repo(user_id, repo_id): return "Repo not found"
+    repo = await database.get_repo(user_id, repo_id)
+    if not repo: return "Repo not found"
       
     installation_id = (await cb.find_one({"_id": int(user_id)})).get('installation_id', None)
     if not installation_id: return "not exists"
     token = await self.gen_token(installation_id)
     
     try:
+      folder = f"deployment{user_id}")
       await database.add_log(user_id, project_id, "Clonning repo from github...")
-      await run(f"rm -rf deployment{user_id}")
-      await run(f"mkdir deployment{user_id}")
+      await run(f"rm -rf {folder}")
+      await run(f"mkdir {folder}")
+      await run(f"git clone https://x-access-token:{token}@github.com/{repo.get('full_name')}/")
+      
       
     
