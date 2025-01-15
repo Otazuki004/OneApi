35dfbcf1d7a53ae44b67fdbcc68b0f6a67d9652d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-15 12:14:56 +0530

    Update check_repo.py

--- OneApi/database/methods/check_repo.py
+++ OneApi/database/methods/check_repo.py
@@ -3,4 +3,19 @@ import traceback
 
 class CheckRepo:
   async def check_repo(self, user_id: int, repo_id: int):
-    
+    try:
+      db, cb = self.db, self.cb
+      user = await self.find(user_id)
+      if not user: return 'not exists'
+      hmm = await self.get_repos(user_id)
+      
+      if not hmm: return False
+      
+      yes = False
+      for x in hmm:
+        if int(x.get('id', 0)) == int(repo_id):
+          yes = True
+          break
+      if yes: return True
+    except: logging.error(traceback.format_exc())
+    return False
