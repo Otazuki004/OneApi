2d1dac7b877ba041c3b260bc66b51a3c73604953
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-15 11:37:39 +0530

    Update deployment.py

--- OneApi/deployment/deployment.py
+++ OneApi/deployment/deployment.py
@@ -1,7 +1,8 @@
 from .. import *
 from . import Methods 
+from ..database import user
 
 class Deployment(Methods):
   def __init__(self):
     self.lf = "\n[ElevenHost]"
-    
+    self.database = user()
