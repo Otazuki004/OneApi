729bf958f4c5734bb4bbb197f77a60b582f42936
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-15 12:02:42 +0530

    Update host.py

--- OneApi/deployment/host.py
+++ OneApi/deployment/host.py
@@ -1,4 +1,6 @@
 class Host:
   async def host(self, user_id: int, project_id: int):
     database = self.database
+    user = await self.find(user_id)
+    project = await database.find(f"p{user_id}{project_id}", project=True)
     
