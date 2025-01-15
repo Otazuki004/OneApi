02771a9a502e5d08374158cb0b31dbbc1b715f2a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-15 12:55:53 +0530

    Update host.py

--- OneApi/api/host.py
+++ OneApi/api/host.py
@@ -1,1 +1,20 @@
+from ..database import *
+from quart import *
+from .. import *
+from ..deployment import Deployment
 
+user = user()
+deploy = Deployment()
+host_bp = Blueprint('host', __name__)
+
+@app.route('/host/', methods=['POST'])
+async def host():
+  data = await request.get_json()
+  if not data or not 'user_id' in data and not 'project_id' in data: return jsonify({'error': 'missing user_id or project_id'}), 400
+  user_id = int(data.get('user_id'))
+  project_id = int(data.get('project_id'))
+
+  mm = await deploy.host(user_id, project_id)
+  if mm is True:
+    return jsonify({'message', 'Successfully hosted!'}), 200
+  return jsonify({'message', mm}), 400
